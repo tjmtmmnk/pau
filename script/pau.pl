a78ebf4f32ee79a5faf28cc04878f6289e28f38b
momochi <tjmtmmnk@gmail.com>
2022-10-02 11:13:57 +0900

    use lib

--- script/pau.pl
+++ script/pau.pl
@@ -1,9 +1,18 @@
 #!/usr/bin/env perl
-use Pau;
-use lib '/app/lib';
-use lib '/cpan/lib/perl5';
-
 # USAGE: docker run --rm -e PAU_LIB_PATH_LIST='lib' -it -v cache-vol:/app/.cache -v (pwd):/src pau:1.0 A.pm
+
+BEGIN {
+    # assure end of path is not /
+    # e.g) lib (not lib/)
+    @lib_path_list = map {
+        (my $path = $_) =~ s/\/$//;
+        $path;
+    } split(/ /, $ENV{PAU_LIB_PATH_LIST});
+}
+
+use lib @lib_path_list;
+
+use Pau;
 
 my $filename  = shift;
 my $formatted = Pau->auto_use($filename);
