c076baa9810e04dce97327258078bd7beaccf2b2
tjmtmmnk <tjmtmmnk@gmail.com>
2023-02-21 08:19:46 +0900

    create pkg for include

--- lib/Pau/Finder.pm
+++ lib/Pau/Finder.pm
@@ -4,6 +4,7 @@ use strict;
 use File::Find qw(find);
 use DDP { show_unicode => 1, use_prototypes => 0, colored => 1 };
 use Module::CoreList;
+use Pau::Util;
 
 my @lib_path_list;
 
@@ -63,18 +64,45 @@ sub find_exported_function {
 
     my $pkg = _filename_to_pkg($filename);
 
+    my $exports = [];
     {
         open my $fh, '>', "/dev/null";
         local *STDOUT             = $fh;
         local *STDERR             = $fh;
         local *CORE::GLOBAL::exit = sub { };
         local *CORE::GLOBAL::die  = sub { };
-        eval "require $pkg";
+        $exports = $class->_exports_for_include($pkg);
     }
     return {
         package   => $pkg,
-        functions => [ @{ $pkg . '::EXPORT' }, @{ $pkg . '::EXPORT_OK' } ],
+        functions => $exports,
     };
+}
+
+# from: https://metacpan.org/release/OALDERS/App-perlimports-0.000050/source/lib/App/perlimports/ExportInspector.pm#L370
+sub _exports_for_include {
+    my ($class, $module_name) = @_;
+    my $pkg     = Pau::Util->pkg_for($module_name);
+    my $to_eval = <<"EOF";
+package $pkg;
+
+use Symbol::Get;
+use $module_name;
+our \@__EXPORTABLES;
+
+BEGIN {
+    \@__EXPORTABLES = (Symbol::Get::get('\@$module_name\::EXPORT')->@*, Symbol::Get::get('\@$module_name\::EXPORT_OK')->@*);
+}
+1;
+EOF
+
+    eval $to_eval;
+
+    no strict 'refs';
+    my $exports = [ grep { $_ !~ m{(?:BEGIN|ISA|__EXPORTABLES)} && $_ !~ m{^__ANON__} } @{ $pkg . '::__EXPORTABLES' } ];
+    use strict;
+
+    return $exports;
 }
 
 sub _filename_to_pkg {
--- lib/Pau/Util.pm
+++ lib/Pau/Util.pm
@@ -1,6 +1,7 @@
 package Pau::Util;
 use strict;
 use warnings;
+use Data::UUID  ();
 use File::Slurp qw(read_file);
 use JSON::XS    qw(encode_json decode_json);
 use Try::Tiny;
@@ -27,6 +28,20 @@ sub write_json_file {
     open my $fh, '>', $filename or die qq/Can't open file "$filename" : $!/;
     print $fh encode_json($data);
     close $fh or die qq/Can't close file $filename: $!/;
+}
+
+# from: https://metacpan.org/module/App::perlimports::Sandbox/source#L11
+sub pkg_for {
+    my ($class, $module_name) = @_;
+    my $du     = Data::UUID->new;
+    my $unique = 'A' . $du->create_str;
+    $unique =~ s{-}{}g;
+
+    return sprintf(
+        'Local::%s::%s',
+        $module_name,
+        $unique
+    );
 }
 
 1;
